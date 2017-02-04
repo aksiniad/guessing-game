72f8d588a0a1578aac9315bdf4365ea32a7cd07c
Aksinnia D <smert.na.diete@gmail.com>
2017-02-04 22:51:53 +0300

    result

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,31 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+    	this.maximum;
+    	this.minimum;
+    	this.result;
+    }
+   
     setRange(min, max) {
-
+    	this.minimum = min+1;
+    	this.maximum = max-1;
     }
 
     guess() {
-
+    	this.result = Math.ceil ((this.maximum + this.minimum) / 2 );
+    	return this.result;
     }
 
     lower() {
-
-    }
+    	this.maximum = this.result - 1;
+    	this.result = Math.ceil ((this.result + this.minimum) / 2);
+    	return this.result;
+    	}
 
     greater() {
-
-    }
+    	this.minimum = this.result +1;
+    	this.result = Math.ceil ((this.result + this.minimum) / 2)/*| 0)+1*/;
+    	return this.result;
+    	}
 }
-
+game = new GuessingGame();
 module.exports = GuessingGame;
